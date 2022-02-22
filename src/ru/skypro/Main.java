5e8e3e9b6095a94c0b742a27eaf5a37726c06661
Ilnur <nabiulli@mail.ru>
2022-02-23 03:49:38 +0500

    Добавил метод для вывода в консоль

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,15 +1,22 @@
 package ru.skypro;
 
 public class Main {
+    public static void printDataBook(String bookName, String authorFirstName, String authorLastName, int publicationYear) {
+        System.out.println(bookName + ", " + authorFirstName + " " + authorLastName + ", " + publicationYear);
+        return;
+    }
 
     public static void main(String[] args) {
         Author authorLordOfRings = new Author("John", "Tolkien");
         Book lordOfTheRings = new Book("Lord of the rings", authorLordOfRings, 1954);
-        System.out.println(lordOfTheRings.getBookName() + ", " + authorLordOfRings.getFirstName() + " " + authorLordOfRings.getLastName() + ", " + lordOfTheRings.getPublicationYear());
+        printDataBook(lordOfTheRings.getBookName(), authorLordOfRings.getFirstName(), authorLordOfRings.getLastName(), lordOfTheRings.getPublicationYear());
+
         Author authorHarryPotter = new Author("Joanne", "Rowling");
         Book harryPotter = new Book("Harry Potter", authorHarryPotter, 1997);
-        System.out.println(harryPotter.getBookName() + ", " + authorHarryPotter.getFirstName() + " " + authorHarryPotter.getLastName() + ", " + harryPotter.getPublicationYear());
+        printDataBook(harryPotter.getBookName(), authorHarryPotter.getFirstName(), authorHarryPotter.getLastName(), harryPotter.getPublicationYear());
+
         harryPotter.setPublicationYear(2007);
-        System.out.println(harryPotter.getBookName() + ", " + authorHarryPotter.getFirstName() + " " + authorHarryPotter.getLastName() + ", " + harryPotter.getPublicationYear());
+        printDataBook(harryPotter.getBookName(), authorHarryPotter.getFirstName(), authorHarryPotter.getLastName(), harryPotter.getPublicationYear());
+
     }
 }
